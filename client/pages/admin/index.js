12968db3b12b65b0daff0e7cd70b9674ad088e26
AneeshaRama <aneesharama@gmail.com>
2022-06-13 11:43:06 +0530

    Initiated the project

---
+++ client/pages/admin/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <>
+      <h1>Admin</h1>
+    </>
+  );
+};
+
+export default index;
---
+++ client/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "antd";
+
+const index = () => {
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  );
+};
+
+export default index;
