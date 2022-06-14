89e9f969050d46ed59f564ce79261898f52db735
AneeshaRama <aneesharama@gmail.com>
2022-06-14 17:44:31 +0530

    Completed category CRUD in backend

--- server/controllers/category.js
+++ server/controllers/category.js
@@ -10,6 +10,40 @@ export const createCategory = async (req, res) => {
     }).save();
     res.status(201).json({ message: "Added new category", category });
   } catch (error) {
-    res.status(500).send(error.message);
+    res.status(500).json({ message: "Failed to add category" });
+  }
+};
+
+export const listAllCategories = async (req, res) => {
+  try {
+    const categories = await Category.find().sort({ createdAt: -1 });
+    res.status(200).json({ categories });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to list all categories" });
+  }
+};
+
+export const deleteCategory = async (req, res) => {
+  try {
+    const { slug } = req.params;
+    const category = await Category.findOneAndDelete({ slug });
+    res.status(200).json({ message: "Successfully deleted", category });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to delete category" });
+  }
+};
+
+export const updateCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { slug } = req.params;
+    const category = await Category.findOneAndUpdate(
+      { slug },
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.json({ message: "Successfully updated", category });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update category" });
   }
 };
--- server/routes/category.js
+++ server/routes/category.js
@@ -4,8 +4,16 @@ const router = express.Router();
 import { requireLogin, adminCheck } from "../middlewares";
 
 // controllers
-import { createCategory } from "../controllers/category";
+import {
+  createCategory,
+  listAllCategories,
+  deleteCategory,
+  updateCategory,
+} from "../controllers/category";
 
 router.post("/category", requireLogin, adminCheck, createCategory);
+router.get("/categories", listAllCategories);
+router.delete("/category/:slug", requireLogin, adminCheck, deleteCategory);
+router.put("/category/:slug", requireLogin, adminCheck, updateCategory);
 
 export default router;
